618bb2df4f9166277bd4cebeee68fb272da7aa5f
Hailong Yang <hailong.yang@runpod.io>
2025-03-16 22:04:04 -0400

    first commit

---
+++ rp_handler.py
@@ -0,0 +1,20 @@
+import runpod
+import time  
+
+def handler(event):
+    print(f"Worker Start")
+    input = event['input']
+    
+    prompt = input.get('prompt')  
+    seconds = input.get('seconds', 0)  
+
+    print(f"Received prompt: {prompt}")
+    print(f"Sleeping for {seconds} seconds...")
+    
+    # Replace the sleep code with your Python function to generate images, text, or run any machine learning workload
+    time.sleep(seconds)  
+    
+    return prompt 
+
+if __name__ == '__main__':
+    runpod.serverless.start({'handler': handler })
